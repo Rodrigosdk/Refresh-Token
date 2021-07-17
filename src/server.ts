72ec5858f65372cced958b8f55ad40e8c7bcec93
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-07-17 19:50:04 -0300

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { App } from './App';
-import './data/connect';
+import { connecteDB } from './data/connect';
 
 const PORT = parseInt(process.env.PORT, 10) || 3333;
 
 const app = new App(PORT);
+
+connecteDB();
 
 app.listen(() => {
   console.log(`🚀 Server on port ${PORT}`);
