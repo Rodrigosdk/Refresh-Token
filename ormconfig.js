2eac9b3560d4dd5e71f25bad8b6c8e6d6831fded
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-07-20 21:52:32 -0300

    rename variables from ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'dev') {
     migrations: [process.env.TYPEORM_MIGRATION],
     cli: {
       entitiesDir: process.env.TYPEORM_ENTITIES_DIR,
-      migrationsDir: process.env.TYPEORM_MIGRATION_DIT,
+      migrationsDir: process.env.TYPEORM_MIGRATION_DIR,
     },
   };
 } else {
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'dev') {
     migrations: [process.env.TYPEORM_MIGRATION_PRODUCTION],
     cli: {
       entitiesDir: process.env.TYPEORM_ENTITIES_DIR_PRODUCTION,
-      migrationsDir: process.env.TYPEORM_MIGRATION_DIT_PRODUCTION,
+      migrationsDir: process.env.TYPEORM_MIGRATION_DIR_PRODUCTION,
     },
   };
 }
